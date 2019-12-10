b4c0c5106882ad43a400a038e607492d4241f8ed
Jan von der Assen <jvdassen@dizmo.com>
2019-12-10 10:19:47 +0100

    Add test examples

---
+++ test/PcapAnalyserTest.js
@@ -0,0 +1,18 @@
+var path = require('path')
+var PacketEmitter = require('../parser/PcapParser')
+var PacketAnalyser = require('../parser/PortScanAnalyser')
+var testfile = path.resolve(__dirname, 'testdata', 'ebfe0afeea7f5417f340782d8a4d6f83portscan.pcap')
+var outFile = path.resolve(__dirname, 'testdata', 'ebfe0afeea7f5417f340782d8a4d6f83portscan.pcap')
+
+var emitter = new PacketEmitter(testfile)
+var analyser = new PacketAnalyser(emitter, outFile)
+
+setUpAndRun()
+
+async function setUpAndRun () {
+    await analyser.setUp()
+    emitter.startPcapSession(testfile)
+    emitter.on('complete', async () => {
+        await analyser.postParsingAnalysis()
+    })
+}
