4c29815d72027c45ce8f21521bbb7945d4c1e5cd
Jan von der Assen <jan.vda@gmx.ch>
2019-12-11 13:50:32 +0100

    Add test

--- frontend/tests/unit/example.spec.js
+++ frontend/tests/unit/example.spec.js
@@ -1,13 +1,13 @@
 import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import HelloWorld from '@/components/BarChart.vue'
 
 describe('BarChart.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+  it('Accepts a dataset as a property', () => {
+    const dataset = [{ port: 80, count: 100 }]
+    const wrapper = mount(HelloWorld, {
+      propsData: { dataset }
     })
-    expect(wrapper.text()).to.include(msg)
+    expect(wrapper.props().dataset).to.include(dataset[0])
   })
 })
