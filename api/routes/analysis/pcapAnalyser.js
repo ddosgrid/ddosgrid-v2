2e83c9c3de8b76bf2f98c232379e35d6d294cf09
Jan von der Assen <jan.vda@gmx.ch>
2019-12-27 13:45:20 +0100

    Use both miners in API

--- api/routes/analysis/pcapAnalyser.js
+++ api/routes/analysis/pcapAnalyser.js
@@ -1,5 +1,5 @@
 //const { PacketEmitter, PortAnalyser} = require('ddosgrid-miner')
-const { PacketEmitter, PortAnalyser, MetricAnalyser} = require('../../../miner')
+const { PacketEmitter, PortAnalyser, MetricAnalyser, PortUsageClusteredAnalyser, TopTwentyPortsByTrafficAnalyser} = require('../../../miner')
 const path = require('path')
 
 
@@ -7,11 +7,13 @@ function analyseFileInProjectFolder (projectPath, cb) {
     var emitter = new PacketEmitter()
     var portAnalyser = new PortAnalyser(emitter, projectPath)
     var metricAnalyser = new MetricAnalyser(emitter, projectPath)
+    var topTwentyanAnalyser = new TopTwentyPortsByTrafficAnalyser(emitter, projectPath)
+    var clusteredAnalyser = new PortUsageClusteredAnalyser(emitter, projectPath)
 
-    setUpAndRun(emitter, portAnalyser, metricAnalyser, projectPath, cb)
+    setUpAndRun(emitter, portAnalyser, metricAnalyser, topTwentyanAnalyser, clusteredAnalyser, projectPath, cb)
 
 }
-async function setUpAndRun (emitter, portAnalyser, metricAnalyser, target, cb) {
+async function setUpAndRun (emitter, portAnalyser, metricAnalyser, topTwentyanAnalyser, clusteredAnalyser, target, cb) {
     await portAnalyser.setUp()
     await metricAnalyser.setUp()
 
@@ -20,10 +22,14 @@ async function setUpAndRun (emitter, portAnalyser, metricAnalyser, target, cb) {
     emitter.on('complete', async () => {
         var portAnalysisResult = await portAnalyser.postParsingAnalysis()
         var metricAnalysisResult = await metricAnalyser.postParsingAnalysis()
+        var topTwentryResult = await topTwentyanAnalyser.postParsingAnalysis()
+        var clusteredResult = await clusteredAnalyser.postParsingAnalysis()
         console.log('Port scan analysis done')
         cb({
           portanalysis: portAnalysisResult,
-          general: metricAnalysisResult
+          general: metricAnalysisResult,
+          topTwenty: topTwentryResult,
+          clusteredPorts: clusteredResult
         })
     })
 }
