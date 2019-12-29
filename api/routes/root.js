b0fb959a09d6149ca445ac4f50b21cd880c9c81a
Jan von der Assen <jvdassen@dizmo.com>
2019-12-29 01:17:09 +0100

    Fix indentation

--- api/routes/root.js
+++ api/routes/root.js
@@ -4,22 +4,24 @@ const router = Router()
 router.get('', getRoot)
 
 function getRoot (_, res) {
-  res.send(`<html>
-              <head>
-                <style>
-                  blockquote {
-                    padding: 0 1em;
-                    color: #6a737d;
-                    border-left: .25em solid #dfe2e5;
-                    margin-left: 0px;
-                  }
-                </style>
-              </head>
-              <body>
-                <h1>ddosgrid API v1</h1>
-                <blockquote>Please refer to the documentation in our <a href="https://github.com/ddosgrid/ddos-visualization">GitHub repository</a>.</blockquote>
-              </body>
-            </html>`)
+  res.send(`
+    <html>
+      <head>
+        <style>
+          blockquote {
+            padding: 0 1em;
+            color: #6a737d;
+            border-left: .25em solid #dfe2e5;
+            margin-left: 0px;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>ddosgrid API v1</h1>
+        <blockquote>Please refer to the documentation in our <a href="https://github.com/ddosgrid/ddos-visualization">GitHub repository</a>.</blockquote>
+      </body>
+    </html>
+  `)
 }
 
 module.exports = router
