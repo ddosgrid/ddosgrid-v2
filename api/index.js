ebe3f807c2e50917932f5a27436a2fa88e275cd9
Jan von der Assen <jan.vda@gmx.ch>
2020-01-02 11:39:32 +0100

    Fix bug in index.js when setting CORS

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ function logStart () {
   console.log(`App is listening on port ${port}`)
 }
 
-function allowCORS () {
+function allowCORS (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://ddosgrid.online")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next();
