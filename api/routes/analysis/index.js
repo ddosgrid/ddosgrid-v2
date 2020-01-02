3d1c8fe6862bbe10858b81264dc6c7ba4a577184
Jan von der Assen <jan.vda@gmx.ch>
2020-01-02 14:20:19 +0100

    Change data model according to issue #28

--- api/routes/analysis/index.js
+++ api/routes/analysis/index.js
@@ -44,22 +44,31 @@ function startAnalysis (req, res) {
     status: 'File was found, analysis should start'
   })
   var projectPath = path.resolve(analysisBaseDir, id, `${id}.pcap`)
-  var filename = `${id}.pcap`
+  var startTime = new Date()
   pcapAnalyser.analyseFileInProjectFolder(projectPath, (analysisResult) => {
-    var portScanFileShort = analysisResult.portanalysis.summaryFile
-    var portScanFileLong = analysisResult.portanalysis.dumpFile
-    var metricScanFile = analysisResult.general.fileName
+    var endTime = new Date()
+    var analysisDurationInSeconds = (endTime - startTime) / 1000
+    var portScanFileShort = analysisResult.portanalysis
+    var topTwenty = analysisResult.topTwenty
+    var clusteredPort = analysisResult.clusteredPorts
+    var metricsScan = analysisResult.general
+    var metrics = analysisResult.general.metrics
     analyses.changeAnalysisStatus(id, 'analysed')
-    var fileName = path.relative(analysisBaseDir, portScanFileShort)
-    analyses.addAnalysisFile(id, fileName)
-    fileName = path.relative(analysisBaseDir, portScanFileLong)
-    analyses.addAnalysisFile(id, fileName)
-    fileName = path.relative(analysisBaseDir, metricScanFile)
-    analyses.addAnalysisFile(id, fileName)
+    analyses.appendMetrics(id, metrics)
+    analyses.storeAnalysisDuration(id, analysisDurationInSeconds)
 
-    // TODO
-    // Store general metrics directly
-    // analyses.addMetricsToDataset(metrics)
+    portScanFileShort.file = path.relative(analysisBaseDir, portScanFileShort.summaryFile)
+    topTwenty.file = path.relative(analysisBaseDir, topTwenty.fileName)
+    clusteredPort.file = path.relative(analysisBaseDir, clusteredPort.fileName)
+    metricsScan.file = path.relative(analysisBaseDir, metricsScan.fileName)
+    var results = [
+      portScanFileShort,
+      topTwenty,
+      clusteredPort,
+      metricsScan
+    ]
+    var cleanedResults = results.map(keepRequiredAttributes)
+    analyses.addAnalysisFiles(id, cleanedResults)
   })
 }
 
@@ -71,23 +80,40 @@ function handleFilePost (req, res) {
   if (req.files.length > 1) {
     return res.status(400).send('Only one file can be uploaded at a time');
   }
-
+  if(!req.body.name || req.body.name == "" ) {
+    return res.status(400).send('No name was given to the dataset')
+  }
+  if(!req.body.description || req.body.description == "") {
+    return res.status(400).send('No description was given to the dataset')
+  }
   var uploadedFile = req.files.captureFile
   if(!uploadedFile) {
     return res.status(400).send('Please upload file with form key "captureFile"');
   }
+  var datasetName = req.body.name
+  var datasetDescription = req.body.description
   var fileHash = uploadedFile.md5
+  var fileSize = uploadedFile.size / 1024 / 1024
+  var fileSizeInMB = Number(Number(fileSize).toFixed(3))
   uploadedFile.mv(path.resolve(analysisBaseDir, fileHash, `${fileHash}.pcap`), mvHandler)
 
   function mvHandler (err) {
     if (err) {
       return res.status(500).send('Error uploading/moving file')
     }
-    analyses.createAnalysis(fileHash)
+    analyses.createAnalysis(fileHash, datasetName, datasetDescription, fileSizeInMB)
     return res.status(200).json({
       id: fileHash,
       status: `You're file was uploaded with ID ${fileHash}`
     })
+  }
+}
+
+function keepRequiredAttributes(element) {
+  return {
+    attackCategory: element.attackCategory,
+    file: element.file,
+    supportedDiagrams: element.supportedDiagrams
   }
 }
 
--- api/routes/analysis/persistence.js
+++ api/routes/analysis/persistence.js
@@ -7,7 +7,7 @@ class Analyses {
     if (instance === null) {
       // load db
       instance = new Datastore({ filename: dbPath, autoload: true })
-    } 
+    }
     // do nothing we can use it as a singleton
   }
   async getAnalyses () {
@@ -22,7 +22,7 @@ class Analyses {
   }
   async getAnalysis (md5) {
     return new Promise((resolve, reject) => {
-      instance.find({id: md5}, (err, analyses) => {
+      instance.find({md5: md5}, (err, analyses) => {
         if (err) {
           reject(err)
         }
@@ -30,21 +30,35 @@ class Analyses {
       })
     })
   }
-  createAnalysis (md5) {
+  createAnalysis (md5, name, description, fileSize) {
     var newAnalysis = {
-      id: md5,
+      md5: md5,
       created: new Date(),
       status: 'uploaded',
-      analysisFiles: []
+      name: name,
+      description: description,
+      fileSizeMB: fileSize,
+      analysisFiles: [],
+      metrics: {}
     }
     instance.insert(newAnalysis)
   }
   changeAnalysisStatus (md5, newStatus) {
-    instance.update({ id: md5 }, { $set: { status: newStatus }})
+    instance.update({ md5: md5 }, { $set: { status: newStatus }})
+  }
+  storeAnalysisDuration(md5, durationInSeconds) {
+    instance.update({md5: md5}, { $set: { analysisDuration: durationInSeconds }})
+  }
+  appendMetrics (md5, metrics) {
+    instance.update({ md5: md5 }, { $set: { metrics: metrics }})
+  }
+
+  addAnalysisFiles (md5, files) {
+    instance.update({ md5: md5 }, { $set: { analysisFiles: files }})
   }
 
   addAnalysisFile (md5, fileName) {
-    instance.update({ id: md5 }, { $push: { analysisFiles: fileName }})
+    instance.update({ md5: md5 }, { $push: { analysisFiles: fileName }})
   }
 }
 
