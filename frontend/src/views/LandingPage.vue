ea10fb1716f77323017a8cab611b2d44d9cba8b0
Luc <luc.boillat@outlook.com>
2020-01-06 01:30:15 +0100

    add github buttone to landing page

--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
   <h1>Hello World</h1>
+  <p>This is a DDoS-Attack visualization web application. The button below will take you to its GitHub page</p>
+  <md-button class="md-raised md-primary" onclick="location.href='https://github.com/ddosgrid/ddos-visualization'">
+    GitHub
+  </md-button>
   <md-button class="md-fab md-primary md-fab-bottom-right" >
     <md-icon>add</md-icon>
   </md-button>
