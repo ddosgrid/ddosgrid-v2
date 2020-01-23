06625bb3781bc56be7ebc5736952accb0dca5869
Jan von der Assen <jan.vda@gmx.ch>
2020-01-23 20:46:33 +0100

    Gibe fance splashe icone

---
+++ frontend/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pwa: {
+    name: 'DDoSGrid',
+    themeColor: '#448aff',
+    iconPaths: {
+      favicon16: '/img/icons/favicon.ico',
+      favicon32: '/img/icons/apple-touch-icon-120x120.png',
+      msTileImage: '/img/icons/apple-touch-icon-152x152.png'
+    }
+  }
+}
