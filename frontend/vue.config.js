13b54f4fa88bd63ba9510090f6b3a363d4893eba
Jan von der Assen <jan.vda@gmx.ch>
2020-01-23 21:06:24 +0100

    Stupid vue cli

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,11 +2,14 @@ module.exports = {
   pwa: {
     name: 'DDoSGrid',
     themeColor: '#448aff',
-    backgroundColor: '#448aff',
     iconPaths: {
       favicon16: '/img/icons/favicon.ico',
       favicon32: '/img/icons/apple-touch-icon-120x120.png',
       msTileImage: '/img/icons/apple-touch-icon-152x152.png'
+    },
+    appleMobileWebAppCapable: true,
+    manifestOptions: {
+      background_color: '#3ddc97'
     }
   }
 }
