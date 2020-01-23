d17ed008b3b2d5e9e1aabea41a29a95bc1c7d0eb
Jan von der Assen <jan.vda@gmx.ch>
2020-01-23 20:59:46 +0100

    Add backgroundcolor to manifest

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   pwa: {
     name: 'DDoSGrid',
     themeColor: '#448aff',
+    backgroundColor: '#448aff',
     iconPaths: {
       favicon16: '/img/icons/favicon.ico',
       favicon32: '/img/icons/apple-touch-icon-120x120.png',
