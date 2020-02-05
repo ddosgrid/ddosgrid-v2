7b1b43a9a7cd6d1e6eec01a202958884dd55b040
Jan von der Assen <jan.vda@gmx.ch>
2020-02-05 15:00:27 +0100

    Case study step1: Mine all addresses and their counts

--- miner/miners/TopNSourceHostsByTraffic.js
+++ miner/miners/TopNSourceHostsByTraffic.js
@@ -4,26 +4,38 @@ const analysisName = 'TBD'
 class SourceHostsAnalyser extends AbstractPcapAnalyser {
     constructor(parser, outPath) {
         super(parser, outPath);
-        this.results = {
+        this.results = [
           // store interim results here
-        }
+          // { addr: '1.2.3.4', count: 1 }
+        ]
     }
 
     // Setup phase, load additional databases, setup subscriptions and signal completion
-    async setUp() {
+    async setUp () {
+        this.pcapParser.on('ipv4Packet', this.countIPv4Address.bind(this))
+    }
+
+    countIPv4Address (ipv4Packet) {
+      var srcAddress = ipv4Packet.saddr.addr.join('.')
+      var existingEntry = this.results.find(item => item.addr === srcAddress)
+
+      if(existingEntry) {
+        existingEntry.count++
+      } else {
+        this.results.push({ addr: srcAddress, count: 1 })
+      }
     }
 
     // Actual mining function
     // Post-analysis phase, do additional computation with the collected data and write it out
     async postParsingAnalysis() {
-
         var fileName = `${this.baseOutPath}-${analysisName}.json`
         var fileContent = {
           // Signal and format to visualize as piechart
           piechart: {
             datasets: [{
               backgroundColor: [],
-              data: Object.values(this.results)
+              data: Object.values(this.results.map(entry => entry.count))
             }],
             labels: []
           }
