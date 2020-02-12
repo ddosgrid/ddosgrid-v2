450cf26c6b663f66220d4bd52ba88062b4fbe407
Jan von der Assen <jan.vda@gmx.ch>
2020-02-12 11:36:17 +0100

    Remove animation

--- frontend/src/components/DataSetTile.vue
+++ frontend/src/components/DataSetTile.vue
@@ -140,40 +140,9 @@ export default {
 .item {
   padding-left: 10px;
 }
-.card {
-  -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
-       -moz-animation: fadein 2s; /* Firefox < 16 */
-        -ms-animation: fadein 2s; /* Internet Explorer */
-         -o-animation: fadein 2s; /* Opera < 12.1 */
-            animation: fadein 2s;
-}
-
 .md-card-actions {
   bottom: 0;
   width: 100%;
   position: absolute;
-}
-
-@keyframes fadein {
-    from { opacity: 0; }
-    to   { opacity: 1; }
-}
-
-/* Firefox < 16 */
-@-moz-keyframes fadein {
-    from { opacity: 0; }
-    to   { opacity: 1; }
-}
-
-/* Safari, Chrome and Opera > 12.1 */
-@-webkit-keyframes fadein {
-    from { opacity: 0; }
-    to   { opacity: 1; }
-}
-
-/* Internet Explorer */
-@-ms-keyframes fadein {
-    from { opacity: 0; }
-    to   { opacity: 1; }
 }
 </style>
