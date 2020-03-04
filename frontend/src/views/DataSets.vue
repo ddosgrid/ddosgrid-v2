26ad15ff67228c73bd54637a4018ca4c99c13872
Jan von der Assen <jan.vda@gmx.ch>
2020-03-04 08:54:52 +0100

    Change upload icon

--- frontend/src/views/DataSets.vue
+++ frontend/src/views/DataSets.vue
@@ -33,7 +33,7 @@
     </div>
 
     <md-button id="fab" @click="showFileUpload = true" class="md-fab md-primary md-fab-bottom-right">
-      <md-icon>add</md-icon>
+      <md-icon>cloud_upload</md-icon>
     </md-button>
 
   </div>
