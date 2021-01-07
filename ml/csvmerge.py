e466f6e19c401df6d8853d89a171a652b6c239d3
lucBoillat <luc.boillat@outlook.com>
2021-01-07 15:19:16 -0500

    add id to addtomodel call

--- ml/csvmerge.py
+++ ml/csvmerge.py
@@ -7,6 +7,9 @@ import pandas as pd
 def main():
     model = pd.read_csv(sys.argv[1])
     toAdd = pd.read_csv(sys.argv[2])
+    id = sys.argv[3]
+
+    toAdd['dataset_id'] = id
 
     newModel = model.append(toAdd)
 
