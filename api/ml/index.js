16e72e50a72a1fe394d97affb1e3c82d4a043a1a
lucBoillat <luc.boillat@outlook.com>
2021-01-09 17:29:07 -0500

    add classification endpoint in /ml

--- api/ml/index.js
+++ api/ml/index.js
@@ -15,6 +15,7 @@ router.get('/algorithms', protect, getAllAlgorithms)
 router.get('/attacktypes', protect, getAllAttackTypes)
 router.post('/:id/addtomodel', protect, addToModel)
 router.post('/:id/removefrommodel', protect, removeFromModel)
+router.post('/:id/classify', protect, startClassification)
 
 async function getAllAlgorithms(req, res) {
   var algorithms = [{
@@ -102,6 +103,69 @@ async function removeFromModel (req, res) {
     })
   } catch (e) {
     return res.status(500).send()
+  }
+}
+
+async function startClassification(req, res) {
+  var id = req.params.id
+  if (!id) {
+    return res.status(404).send('ID not supplied')
+  }
+  var filePath = path.resolve(analysisBaseDir, id, `${id}.pcap-ML-features.csv`)
+  try {
+    fs.statSync(filePath)
+  } catch (e) {
+    return res.status(404).send('ID unknown')
+  }
+  var analysis = await analyses.getAnalysis(id)
+  if(!analysis) {
+    return res.status(404).send('File was found but no corresponding database entry. Check upload?')
+  }
+  var analysisRequestedByUploader = req.user._id === analysis.uploader
+  if(!analysisRequestedByUploader) {
+    return res.status(403).send('Unauthorized to start analysis on a dataset that was uploaded with different account')
+  }
+
+  if (analysis.status !== 'analysed') {
+    return res.status(400).send('Can\'t start classification as analysis has not yet been performed successfully.')
+  }
+
+  if (analysis.classificationStatus === 'classified') {
+    return res.status(400).send('Classification has already been performed.')
+  }
+
+  if (analysis.classificationStatus === 'in progress') {
+    return res.status(400).send('Classification is already in progress.')
+  }
+
+  if (analysis.classificationType = 'auto') {
+    try {
+      console.log(filePath);
+      console.log(analysis.analysisFiles[11].file);
+      var mlResults = await classification.machineLearning(filePath, analysis.algorithm)
+
+      var fullJSONFileName = analysisBaseDir + '/' + analysis.analysisFiles[11].file
+
+      fs.readFile(fullJSONFileName, 'utf8', (err, data) => {
+        if (err) { console.log(`Error reading file from disk: ${err}`); }
+        else {
+          var JSONFeatures = JSON.parse(data);
+          JSONFeatures.linechart.datasets[0].data = mlResults
+
+          fs.writeFile(fullJSONFileName, JSON.stringify(JSONFeatures), 'utf8', (err) => {
+              if (err) {
+                console.log(`Error writing file: ${err}`);
+              } else {
+                console.log(`File is written successfully!`);
+              }
+            });
+        }
+        analyses.changeClassificationStatus(id, 'classified')
+      });
+    } catch (e) {
+      analyses.changeClassificationStatus(id, 'failed')
+
+    }
   }
 }
 
