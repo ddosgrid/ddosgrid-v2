c1d7f914fe99882475b1bf44f646353e780b0ea3
lucBoillat <luc.boillat@outlook.com>
2021-01-09 18:41:27 -0500

    allow specifying attack types for miners

--- api/analysis/pcapAnalyser.js
+++ api/analysis/pcapAnalyser.js
@@ -2,10 +2,11 @@ const child_process = require('child_process')
 const fork = child_process.fork
 const path = require('path')
 
-async function analyseFileInProjectFolder (pcapPath) {
+async function analyseFileInProjectFolder (pcapPath, attackType) {
   return new Promise(function (resolve, reject) {
     var program = path.resolve('../miner/index.js')
-    var args = [ `pcap_path=${pcapPath}` ]
+    var args = [ `pcap_path=${pcapPath}`,
+                `attack_type=${attackType}` ]
     var options = { stdio: [ 'ipc' ] }
 
     var childProcess = fork(program, args, options)
