f42ab0cbbdee6b5edf85f73ffb374d561f513965
lucBoillat <luc.boillat@outlook.com>
2021-03-11 22:23:42 +0100

    prepare eval scripts

--- ml/evaluation/performance/knn_eval.py
+++ ml/evaluation/performance/knn_eval.py
@@ -6,8 +6,19 @@ import numpy as np
 import pandas as pd
 from timeit import default_timer as timer
 
-
 def main():
+    print('Running Evaluation 10 Times...')
+    times = [0, 0, 0]
+    for x in range(10):
+        evaluate()
+    print('Avg Total Speed')
+    print(times[0] / 10)
+    print('Avg Import and Model Building Speed')
+    print(times[1] / 10)
+    print('Avg Classification Speed')
+    print(times[2] / 10)
+
+def evaluate():
     start = timer()
     dataset = pd.read_csv(sys.argv[1])
     X = dataset.iloc[:, 2:-2].values
@@ -32,12 +43,7 @@ def main():
     pd.DataFrame(toPredict).to_csv(sys.argv[2], index=False)
 
     end = timer()
-    print('Full')
-    print(end - start) # Time in seconds, e.g. 5.38091952400282
 
-    print('First Half')
-    print(middle - start)
-
-    print('Second Half')
-    print(end - middle)
-main()
+    times[0] += end - start
+    times[1] += middle - start
+    times[2] += end - middle
