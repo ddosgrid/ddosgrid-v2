4908baa59452203b2a6f69108446a0961a7423bc
lucBoillat <luc.boillat@outlook.com>
2021-03-11 22:14:18 +0100

    cleanup

--- ml/addtomodel.py
+++ ml/addtomodel.py
@@ -7,6 +7,9 @@ import pandas as pd
 def main():
     model = pd.read_csv(sys.argv[1])
     toAdd = pd.read_csv(sys.argv[2])
+
+    # toAdd.drop_duplicates(subset=toAdd.columns.difference(['num_packets']), inplace=True)
+
     id = sys.argv[3]
 
     toAdd['dataset_id'] = id
