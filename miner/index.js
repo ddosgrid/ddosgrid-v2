b5013cc0267d7043f4a2e655fb802b3ef0f7bad2
Jan von der Assen <jan.vda@gmx.ch>
2021-06-23 21:17:45 +0200

    Clean up

--- miner/index.js
+++ miner/index.js
@@ -25,7 +25,7 @@ try {
   console.log('✓ Input check completed')
   if(settings.live) {
     var { targetInterface } = settings
-    var projectPath = `${process.cwd()}/live-analysis-${targetInterface}-${Date.now()}`
+    var projectPath = `${process.cwd()}/results/live-analysis-${targetInterface}-${Date.now()}`
     analyseFromInterface(targetInterface)
   } else {
     analyseFileInProjectFolder(settings.pcapPath)
@@ -99,29 +99,6 @@ async function setUpAndRun (emitter, activeMiners, target) {
     finalizeAnalysis(decodingTimer, activeMiners)
   })
 }
-async function finalizeAnalysis (decodingTimer, activeMiners) {
-  var decodingDuration = (new Date() - decodingTimer) / 1000 + 's'
-  console.log(`\n✓ Decoding has finished (${decodingDuration.green}), starting post-parsing analysis`)
-  // var results = activeMiners.map(async (miner) => { return await miner.postParsingAnalysis() })
-  console.log('✓ Post-parsing analysis of the following miners has completed:')
-  var results = []
-  for (var miner of activeMiners) {
-    let startTimer = new Date()
-    var result = await miner.postParsingAnalysis()
-    results.push(result)
-    let duration = (new Date() - startTimer) / 10000
-    console.log(`\t- (${duration}s) \t${miner.getName()}`)
-  }
-  console.log('✓ All miners have finished.')
-  var output = JSON.stringify(results)
-  if (process && process.send) {
-    // If this function exists in scope we know that we are in a forked ChildProcess
-    // This will then send the output of the miners over IPC to the master process
-    process.send(output)
-  } else {
-    console.log(output)
-  }
-}
 
 async function setUpAndRunLive (emitter, activeMiners, target) {
   // The NodeJS version used (10) does not support Promise.map
@@ -140,13 +117,50 @@ async function setUpAndRunLive (emitter, activeMiners, target) {
     emitter.startLiveSession(target)
     console.log(`✓ Decoding has started...`)
 
+    process.on('SIGTERM', () => {
+      emitter.closeLiveSession()
+      finalizeAnalysis(decodingTimer, activeMiners)
+    })
+    process.on('SIGINT', () => {
+      emitter.closeLiveSession()
+      finalizeAnalysis(decodingTimer, activeMiners)
+    })
+    /*
     setTimeout(async () => {
       emitter.closeLiveSession()
       finalizeAnalysis(decodingTimer, activeMiners)
-    }, 3000)
+    }, 60000)
+    */
   } catch (e) {
     console.error(e)
     process.exit(1)
   }
+}
 
+async function finalizeAnalysis (decodingTimer, activeMiners) {
+  var decodingDuration = (new Date() - decodingTimer) / 1000 + 's'
+  console.log(`\n✓ Decoding has finished (${decodingDuration.green}), starting post-parsing analysis`)
+  // var results = activeMiners.map(async (miner) => { return await miner.postParsingAnalysis() })
+  console.log('✓ Post-parsing analysis of the following miners has completed:')
+  var results = []
+  var postParsingStart = new Date()
+  for (var miner of activeMiners) {
+    let startTimer = new Date()
+    var result = await miner.postParsingAnalysis()
+    results.push(result)
+    let duration = (new Date() - startTimer) / 10000
+    console.log(`\t- (${duration}s) \t${miner.getName()}`)
+  }
+  var postParsingDuration = (new Date() - postParsingStart) / 1000 + 's'
+  console.log('\t==================================================================')
+  console.log(`\tTotal: ${postParsingDuration}`)
+  console.log('✓ All miners have finished.\n')
+  var output = JSON.stringify(results)
+  if (process && process.send) {
+    // If this function exists in scope we know that we are in a forked ChildProcess
+    // This will then send the output of the miners over IPC to the master process
+    process.send(output)
+  } else {
+    console.log(output)
+  }
 }
