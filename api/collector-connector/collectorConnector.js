611d5e1c47dad1da3e549d0fc66fbc87f9a0f673
Iodas <michi.michi@outlook.com>
2021-10-14 17:56:46 +0200

    added comments for pcap file replay and monitoring of port

--- api/collector-connector/collectorConnector.js
+++ api/collector-connector/collectorConnector.js
@@ -1,8 +1,30 @@
+/*
+In order to replay certain pcap files to a specific port as outpout i used this:
+https://tcpreplay.appneta.com/
+https://tcpreplay.appneta.com/wiki/tcpreplay-edit-man.html
+example command here:
+sudo tcpreplay-edit -i wlo1 -t -K --portmap=1-9999:3000 DNS-flooding_7498de6f87aea566dadd7c2302eaf59f.pcap
+
+To see traffic on port:
+sudo tcpdump port 3000 and '(tcp-syn|tcp-ack)!=0'
+ */
+
+
 var Collector = require('node-netflowv9');
-class CollectorConnector {
 
+module.exports.init = function() {
+    console.log("listening...")
+    var collector = Collector({port: 3000});
+    collector.on('data', function(data) {
+        console.log(data);
+    });
+    collector.on('template', function(data) {
+        console.log(data);
+    });
 
-    // Collector({port: 3000}).on('data',function(flow) {
-    //     console.log(flow);
-    // });
 }
+
+// class CollectorConnector {
+//     constructor() {
+//     }
+// }
