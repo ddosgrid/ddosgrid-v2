4ee9626f92e9247475de3727453875b5713f74e1
Iodas <michi.michi@outlook.com>
2021-11-02 18:21:11 +0100

    spacing

--- api/collector-connector/collectorConnector.js
+++ api/collector-connector/collectorConnector.js
@@ -12,7 +12,8 @@ sudo tcpdump port 3000 and '(tcp-syn|tcp-ack)!=0'
 Tool used for Netflow exporter(WHICH IS ALSO ABLE TO REPLAY PCAP FILES): softflowd
 softflowd -n localhost:9000 -r ddosdb_syn_2-chunked.pcap
 softflowd -n localhost:3000 -r DNS-flooding_7498de6f87aea566dadd7c2302eaf59f.pcap
-to monitor a port and aggregate data to flow with different pcap file replayer example: sudo softflowd -n 0.0.0.0:3000 -i wlo1
+to monitor a port and aggregate data to flow with different pcap file replayer example:
+sudo softflowd -n 0.0.0.0:3000 -i wlo1
  */
 
 
@@ -25,8 +26,3 @@ module.exports.init = function() {
         console.log(data);
     });
 }
-
-// class CollectorConnector {
-//     constructor() {
-//     }
-// }
