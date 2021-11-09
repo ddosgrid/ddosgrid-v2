a281f200f7a07aa5ae43f79f291beee2e647827c
SandroPadovan <sandropadovan@bluewin.ch>
2021-11-09 17:31:31 +0100

    extended api to GET and DELETE connections, plus better error handling

--- api/live-analysis/index.js
+++ api/live-analysis/index.js
@@ -7,8 +7,34 @@ const manager = new Manager()
 
 const {protect} = require('../auth/index')
 
+router.get('/connection', protect, getAllLiveConnections)
 router.post('/connection', protect, bodyParser.json(), establishConnection)
+router.delete('/connection', protect, deleteConnection)
 
+const connections = new Set()
+
+async function getAllLiveConnections(req, res) {
+  try {
+    all = [...connections]
+    res.json(all)
+  } catch (e) {
+    console.warn(e)
+    res.send(500)
+  }
+}
+
+function connectionAlreadyExists(port) {
+  // check if a connection with the same port already exists
+  // connections.has() does not work here
+  let exists = false
+  connections.forEach((connection) => {
+    if (connection.port === port) {
+      // can't return out of forEach loop
+      exists = true
+    }
+  })
+  return exists
+}
 
 async function establishConnection(req, res) {
   try {
@@ -16,12 +42,33 @@ async function establishConnection(req, res) {
       return res.status(400).send('No body sent')
     }
     if (!req.body.hasOwnProperty('port') || !req.body.port || req.body.port === '') {
-      return res.status(400).send('No port was sent')
+      return res.status(400).send('No port was sent.')
     }
-    const port = req.body.port
-    await manager.establishConnection(port)
+    if (isNaN(parseInt(req.body.port))) {
+      return res.status(400).send('Port must be a number.')
+    }
+    const port = parseInt(req.body.port)
 
-    res.status(200).send(`Collector is connected on port ${port}`)
+    if (connectionAlreadyExists(port)) {
+      return res.status(400).send('Connection to this port already exists.')
+    }
+    await manager.establishConnection(port)
+    connections.add({'port': port})
+    res.status(200).send(`Collector is connected on port ${port}.`)
+  } catch (e) {
+    console.warn(e)
+    res.send(500)
+  }
+}
+
+function deleteConnection(req, res) {
+  try {
+    connections.forEach((connection) => {
+      if (connection.port === parseInt(req.query.PORT)) {
+        connections.delete(connection)
+      }
+    })
+    res.status(200).send('deleted')
   } catch (e) {
     console.warn(e)
     res.send(500)
