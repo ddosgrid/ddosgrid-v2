4a85876c09cde812da5769efe33c6660cd1fa735
Iodas <michi.michi@outlook.com>
2021-11-09 13:56:20 +0100

    added eventemitter and basic collector class

--- api/collector-connector/collectorConnector.js
+++ api/collector-connector/collectorConnector.js
@@ -10,19 +10,42 @@ sudo tcpdump port 3000 and '(tcp-syn|tcp-ack)!=0'
 
 
 Tool used for Netflow exporter(WHICH IS ALSO ABLE TO REPLAY PCAP FILES): softflowd
-softflowd -n localhost:9000 -r ddosdb_syn_2-chunked.pcap
+softflowd -n localhost:3000 -r UDP-flooding_fcada352e9c25e65977abfdaf40eae6d.pcap
 softflowd -n localhost:3000 -r DNS-flooding_7498de6f87aea566dadd7c2302eaf59f.pcap
 to monitor a port and aggregate data to flow with different pcap file replayer example:
 sudo softflowd -n 0.0.0.0:3000 -i wlo1
  */
 
+let netflowCollector = require('node-netflowv9');
+const EventEmitter = require('events')
+const dataBroadcaster = new EventEmitter()
 
-var Collector = require('node-netflowv9');
+class Collector {
+    constructor(port) {
+        this.port = port;
+    }
+}
+
+class NetflowCollector extends Collector {
+    constructor(...args) {
+        super(...args);
+        this.collector = netflowCollector({port: this.port})
+    }
+    start(){
+        this.collector.on("data", function (flow){
+            console.log(flow);
+            dataBroadcaster.emit(flow)
+        })
+    }
+
+    stop(){
+
+    }
+}
+
 
 module.exports.init = function() {
     console.log("listening...")
-    var collector = Collector({port: 3000});
-    collector.on('data', function(data) {
-        console.log(data);
-    });
+    let collector = new NetflowCollector({port: 3000})
+    collector.start();
 }
