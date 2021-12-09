1c079c9cf5c0a9dad613d45a4a3f1899107bc646
Christian Birchler <christian.birchler2@uzh.ch>
2021-12-10 00:19:31 +0100

    mine total number of in_bytes

--- miner/live-miners/SampleConcreteLiveMiner.js
+++ miner/live-miners/SampleConcreteLiveMiner.js
@@ -1,17 +1,29 @@
-import AbstractLiveMiner from './AbstractLiveMiner'
+let AbstractLiveMiner = require('./AbstractLiveMiner')
 
-class SampleConcreteLiveMiner extends AbstractLiveMiner {
-    constructor(collectorConnector) {
-        super(collectorConnector)
+class SampleConcreteLiveMiner {
+    constructor(dataBroadcaster) {
+        this.setUp(dataBroadcaster)
     }
 
-    setUp() {
-
+    setUp(dataBroadcaster) {
+        console.log('miner: setup')
+        dataBroadcaster.on('data', (data) => { this.miningTotalBytes(data) })
     }
 
     startStreaming() {
         this.collectorConnector
     }
+
+    miningTotalBytes(data) {
+        console.log('mining total number bytes')
+        let flows = data.flows
+        let tot_in_bytes = 0
+        for(let i in flows) {
+            //console.log(flows[i])
+            tot_in_bytes += flows[i].in_bytes
+        }
+        console.log(tot_in_bytes)
+        }
 }
 
 module.exports = SampleConcreteLiveMiner
