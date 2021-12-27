ca9dd93fa46e3f2e12450dfe61c230da2a7dcbdb
SandroPadovan <sandropadovan@bluewin.ch>
2021-12-27 09:18:00 +0100

    added LineChart component for live analysis

---
+++ frontend/src/components/LiveLineChart.vue
@@ -0,0 +1,29 @@
+<script>
+import { Line, mixins } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  mixins: [mixins.reactiveProp],
+  props: ['options'],
+  mounted: function () {
+    try {
+      // If the data of the response is formatted to be rendered directly we
+      // can directly feed it to the renderChart method!
+      this.renderChart(this.chartData, this.options)
+    } catch (e) {
+      console.log(e)
+      this.$emit('unavailable')
+    }
+  },
+  methods: {
+    update () {
+      this.renderChart(this.chartData, this.options)
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
