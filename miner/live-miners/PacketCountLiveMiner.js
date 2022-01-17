3513fc88e16ae435260dc2c442b6925d3e25925d
Christian Birchler <christian.birchler2@uzh.ch>
2022-01-17 09:36:19 +0100

    implement `PacketCountMiner`
    This miner counts the total number of packets withing the flow packet.

---
+++ miner/live-miners/PacketCountLiveMiner.js
@@ -0,0 +1,27 @@
+const { socketBroadcaster } = require('../../api/live-analysis/SocketHandler')
+
+class PacketCountLiveMiner {
+  constructor (dataBroadcaster) {
+    this.dataBroadcaster = dataBroadcaster
+    this.setUp()
+  }
+
+  setUp () {
+    console.log('miner: setup')
+    this.dataBroadcaster.on('data', (data) => { this.mining(data) })
+  }
+
+  mining (data) {
+    console.log('mining total number packets')
+    const flows = data.flows
+    let totInPackets = 0
+    for (const i in flows) {
+      totInPackets += flows[i].in_pkts
+    }
+    const res = { total_in_packets: totInPackets, timestamp: Date.now() }
+    console.log(res)
+    socketBroadcaster.emit('newData', res)
+  }
+}
+
+module.exports = PacketCountLiveMiner
