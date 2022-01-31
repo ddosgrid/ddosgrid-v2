0020081b97f96d0f130d3609be665262b40d693f
Christian Birchler <christian.birchler2@uzh.ch>
2022-01-31 10:29:39 +0100

    Emit aggregated data in intervals

--- miner/live-miners/ByteCountLiveMiner.js
+++ miner/live-miners/ByteCountLiveMiner.js
@@ -3,7 +3,21 @@ const { socketBroadcaster } = require('../../api/live-analysis/SocketHandler')
 class ByteCountLiveMiner {
   constructor (dataBroadcaster) {
     this.dataBroadcaster = dataBroadcaster
+    this.aggregatedData = null
+    this.timer = null
     this.setUp()
+    setInterval(this.emitAggregatedData.bind(this), 3000)
+  }
+
+  emitAggregatedData () {
+    console.log('emitAggregatedData')
+    if (this.aggregatedData) {
+      socketBroadcaster.emit('newData', this.aggregatedData)
+      console.log(this.aggregatedData)
+      // TODO: dump data for evaluation
+    } else {
+      console.log('no aggregated data')
+    }
   }
 
   setUp () {
@@ -11,17 +25,25 @@ class ByteCountLiveMiner {
     this.dataBroadcaster.on('data', (data) => { this.mining(data) })
   }
 
+  aggregateMinedData (minedData) {
+    // console.log('aggregateMinedData')
+    if (!this.aggregatedData) {
+      this.aggregatedData = minedData
+    } else {
+      this.aggregatedData.total_in_bytes += minedData.total_in_bytes
+      this.aggregatedData.timestamp = Date.now()
+    }
+  }
+
   mining (data) {
-    console.log('mining total number bytes')
+    // console.log('mining total number bytes')
     const flows = data.flows
     let totInBytes = 0
     for (const i in flows) {
-      // console.log(flows[i])
       totInBytes += flows[i].in_bytes
     }
     const res = { miner: 'ByteCount', total_in_bytes: totInBytes, timestamp: Date.now() }
-    console.log(res)
-    socketBroadcaster.emit('newData', res)
+    this.aggregateMinedData(res)
   }
 }
 
